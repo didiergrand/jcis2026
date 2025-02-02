1800a131b6e28ed0b628ac3cf4ff8323c8e0bcc4
Didier Grand <didier.grand@gmail.com>
2025-02-02 16:11:54 +0100

    add billetterie page and menu item

---
+++ archive-billetterie.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Archive Billetterie
+ * Description: Page qui affiche tous les posts de la catégorie billetterie
+ */
+
+get_header(); ?>
+
+
+    <!-- Tickets Section -->
+    <?php get_template_part('template-parts/sections/tickets'); ?>
+<?php get_footer(); ?> 
