3c77d3df86878be8a43dc979871da1dfb00bdc12
Didier Grand <didier.grand@gmail.com>
2025-02-03 13:31:11 +0100

    Add translations logic

--- template-parts/sections/showtime.php
+++ template-parts/sections/showtime.php
@@ -1,8 +1,14 @@
 <?php
-// Query posts with the 'herobanner' category
+// Déterminer la catégorie en fonction de la langue active
+$lang = pll_current_language(); // Récupère la langue active
+
+
+$category_slug = ($lang === 'de') ? 'showtime-de' : 'showtime';
+
+// Récupérer les articles de la catégorie correspondante
 $hero_banner_query = new WP_Query(array(
-    'category_name' => 'showtime',
-    'posts_per_page' => 1, // Limit to one post
+    'category_name' => $category_slug,
+    'posts_per_page' => 4,
 ));
 
 if ($hero_banner_query->have_posts()) :
--- template-parts/sections/ticker-banner.php
+++ template-parts/sections/ticker-banner.php
@@ -1,4 +1,136 @@
-<section class="ticker-wrapper">
+<?php $lang = pll_current_language(); // Récupère la langue active
+
+if ($lang === 'de') {?>
+    <section class="ticker-wrapper">
+    <div class="ticker-container">
+        <div class="ticker-content">
+            <div class="ticker-banner">
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span>Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span>Motor Aktion!</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Châtel-Saint-Denis</span>
+            <span class="star-icon"></span>
+            <span>20. und 21. März 2026</span>
+            <span class="star-icon"></span>
+            <span class="ticker-outline">Motor Aktion!</span>
+        </div>
+    </div>
+</div>
+</section>
+<?php } else { ?>
+    <section class="ticker-wrapper">
     <div class="ticker-container">
         <div class="ticker-content">
             <div class="ticker-banner">
@@ -126,3 +258,4 @@
     </div>
 </div>
 </section>
+<?php } ?>
